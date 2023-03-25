a001d9ed671c8923521f7876ec031f1acbae6970
jonnebonde <jonnebonde@gmail.com>
2023-03-25 21:26:14 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,24 @@
 import { games } from "../script.js";
 
-
-console.log(games)
-
 function renderGames() {
-  const resultContainer = document.querySelector(".results__container")
+  const resultContainer = document.querySelector(".results__container");
 
   games.forEach((games) => {
     resultContainer.innerHTML += `<div class="games__container">
     <h4>${games.name}</h4>
     <span>${games.rating}</span>
-    </div>`
-  })
+    </div>`;
+  });
 
-  const gamesToggle = document.querySelectorAll(".games__container")
+  const gamesToggle = document.querySelectorAll(".games__container");
 
   gamesToggle.forEach((btn) => {
     btn.addEventListener("click", togglePlayed);
-  })
-
+  });
 }
 
-renderGames()
-
+renderGames();
 
 function togglePlayed() {
- this.classList.toggle("played")
+  this.classList.toggle("played");
 }
