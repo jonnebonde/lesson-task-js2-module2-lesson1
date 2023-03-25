be0f333e6e4e9275e185eb437ffc01353b8f2895
jonnebonde <jonnebonde@gmail.com>
2023-03-25 14:22:24 +0100

    initial commit and finished task

---
+++ js/index.js
@@ -0,0 +1,29 @@
+import { games } from "../script.js";
+
+
+console.log(games)
+
+function renderGames() {
+  const resultContainer = document.querySelector(".results__container")
+
+  games.forEach((games) => {
+    resultContainer.innerHTML += `<div class="games__container">
+    <h4>${games.name}</h4>
+    <span>${games.rating}</span>
+    </div>`
+  })
+
+  const gamesToggle = document.querySelectorAll(".games__container")
+
+  gamesToggle.forEach((btn) => {
+    btn.addEventListener("click", togglePlayed);
+  })
+
+}
+
+renderGames()
+
+
+function togglePlayed() {
+ this.classList.toggle("played")
+}
